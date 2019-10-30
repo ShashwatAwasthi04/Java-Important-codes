56e2a76a13032bccafa9bc8ae4865a00ec4e98c8
ShashwatAwasthi04 <53440849+ShashwatAwasthi04@users.noreply.github.com>
2019-10-30 12:55:44 +0530

    Add files via upload

--- HW2.java
+++ HW2.java
@@ -2,13 +2,14 @@ import java.util.*;
 class HW2{
 public static void main(){
 Scanner sc=new Scanner(System.in);
-System.out.println("Enter Array Size");
+System.out.println("Enter Array Size");//Entering Array Size
 int n=sc.nextInt();
 int A[]=new int[n];
 System.out.println("Enter Array Nos");
     for(int i=0;i<n;i++)
         A[i]=sc.nextInt();
-int t=0;System.out.println("Enter Array Rotation Number");int q=sc.nextInt();
+int t=0;System.out.println("Enter Array Rotation Number");//Entering from where to be rotated
+int q=sc.nextInt();
 for(int k=0;k<q;k++){t=A[0];
     for(int j=0;j<n-1;j++){
     A[j]=A[j+1];
